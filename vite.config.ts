ea210b1fec55a3286efcd9109d71af76b0e34663
Nikkoola22 <165650747+Nikkoola22@users.noreply.github.com>
2025-09-21 19:45:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode } ) => ({
+  base: "/pause-magique/", // Ajouté pour le déploiement sur GitHub Pages
   server: {
     host: "::",
     port: 8080,
